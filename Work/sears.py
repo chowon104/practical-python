d2ce3bb68a998516317a50d5b6093948c377382e
hy43 <hyde0401@gmail.com>
2025-04-05 13:18:14 +0900

    debug : change name days -> day

--- Work/sears.py
+++ Work/sears.py
@@ -1,9 +1,11 @@
-bill_thickness = 0.11 * 0.001
-sears_height = 442
-num_bills = 1
-day = 1
+# sears.py
 
-while num_bills * bill_thickness < sears_height :
+bill_thickness = 0.11 * 0.001    # 미터(0.11 mm)
+sears_height   = 442             # 높이(미터)
+num_bills      = 1
+day            = 1
+
+while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
     day = day + 1
     num_bills = num_bills * 2
@@ -11,3 +13,4 @@ while num_bills * bill_thickness < sears_height :
 print('Number of days', day)
 print('Number of bills', num_bills)
 print('Final height', num_bills * bill_thickness)
+
