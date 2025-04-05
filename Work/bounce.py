bbae058df0ff6d2a28c07ad967e2791c4f85b321
hy43 <hyde0401@gmail.com>
2025-04-05 13:02:29 +0900

    feat : bouce ball

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+bound_cnt = 0
+height = 100
+while bound_cnt < 10 :
+    height = height * (3/5)
+    print(bound_cnt + 1, round(height, 4))
+    bound_cnt = bound_cnt +1 
