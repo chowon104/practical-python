748a0d5f3df46cfe9cf61f741e1d4a86d53ba30c
hy43 <hyde0401@gmail.com>
2025-04-06 12:55:48 +0900

    feat : mortgage 1.0 ver

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,14 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000
+fixed_rate = 0.05
+paymemt = 2684.11
+total_paid = 0.0
+
+while principal > 0 :
+    principal = principal * (1 + fixed_rate/12) - paymemt
+    total_paid = total_paid + paymemt
+
+print('Total paid', total_paid)
